05eb54cecdb179ee4ddf99774023307267404744
Michael S. Christensen <michael.stenskjaer@gmail.com>
2014-07-16 12:29:51 +0200

    Birth of settings file

---
+++ scripts/settings.py
@@ -0,0 +1,25 @@
+# author: Author name
+# corpus_dir: Directory containing correctly formatted corpora
+# corpus_subdir: Optional subdir used in stead of author as corpus dir
+# filename_addon: Ending of corpus filename
+author = ""
+corpus_dir = ""
+corpus_subdir = ""
+filename_addon = ""
+
+
+# terms: Søgetermer, liste. Formatteret: [' ανδρει',' ηνορε',' αδροτ',' ανδρειοτ']
+# exceptions: Søgetermer som skal udelukkes. Formatteres som søgetermer.
+# bar_baseline: Koordinat for diagramlinjens grundlinje.
+terms = []
+exceptions = []
+bar_baseline = ""
+
+# freq_factor: Frekvensfaktor anvendt i statistiske beregninger
+# width_factor: Bredden på diagrammet
+# diagram_bars: Antal diagramlinjer
+# granularity: Hvor fint skal den skelne mellem linjernes placering?
+freq_factor = 10000
+width_factor = 10
+diagram_bars = 7
+granularity = 4
