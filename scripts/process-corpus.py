83191849944c4c8ee2a6f1699b25b2e9d080ba42
Michael S. Christensen <michael.stenskjaer@gmail.com>
2014-08-31 21:02:31 +0200

    open_file simplified: no need for settings input

--- scripts/process-corpus.py
+++ scripts/process-corpus.py
@@ -4,31 +4,16 @@ import unicodedata
 import os, sys, argparse, codecs, subprocess, shutil
 import settings
 
-def open_file(filename=False):
+def open_file(filename, encoding='utf8'):
     """
     Open file and load content into string.
     
     Returns the content as decoded string.
     """
 
-    # Load vars from settings
-    corpus_dir 		= settings.corpus_dir
-    corpus_subdir 	= settings.corpus_subdir
-    author 		= settings.author
-    filename_addon	= settings.filename_addon
-    # If filename_prefix is not set, use author
-    filename_prefix	= settings.filename_prefix if settings.filename_prefix else author 
-    
-    # If filename is not set from command line parameters, use settings
-    if not filename:
-        if settings.corpus_subdir:
-            filename = os.path.join(corpus_dir, corpus_subdir, filename_prefix+filename_addon)
-        else:
-            filename = os.path.join(corpus_dir, author, filename_prefix+filename_addon)
-
     # Try opening the file
     try:
-        f = codecs.open(filename, 'rb','utf8')
+        f = codecs.open(filename, 'rb', encoding)
         s = f.read()
     except IOError as err:
         print('Cannot open {}'.format(err))
