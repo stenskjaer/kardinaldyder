f60da962cbd688a3d4e4ad3d349677c9e74aede2
Michael S. Christensen <michael.stenskjaer@gmail.com>
2014-08-31 09:28:07 +0200

    include open_file function

--- scripts/process-corpus.py
+++ scripts/process-corpus.py
@@ -1,9 +1,44 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import common
 import unicodedata
 import argparse
 import os, logging
+
+def open_file(filename=False):
+    """
+    Open file and load content into string.
+    
+    Returns the content as decoded string.
+    """
+
+    # Load vars from settings
+    corpus_dir 		= settings.corpus_dir
+    corpus_subdir 	= settings.corpus_subdir
+    author 		= settings.author
+    filename_addon	= settings.filename_addon
+    # If filename_prefix is not set, use author
+    filename_prefix	= settings.filename_prefix if settings.filename_prefix else author 
+    
+    # If filename is not set from command line parameters, use settings
+    if not filename:
+        if settings.corpus_subdir:
+            filename = os.path.join(corpus_dir, corpus_subdir, filename_prefix+filename_addon)
+        else:
+            filename = os.path.join(corpus_dir, author, filename_prefix+filename_addon)
+
+    # Try opening the file
+    try:
+        f = codecs.open(filename, 'rb','utf8')
+        s = f.read()
+    except IOError as err:
+        print('Cannot open {}'.format(err))
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+    else:
+        f.close()
+
+        logging.info('String succesfully read')
+        return(s)
 
 def file_list(question):
     """Determine whether input is file or directory and load files
