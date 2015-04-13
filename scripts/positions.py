3b6b310d14d29eb7cc2d83ed5e12db2621a30cc8
Michael Stenskjær Christensen <michael@michael-2.local>
2015-04-13 12:42:13 +0200

    Add the missing open_file function

--- scripts/positions.py
+++ scripts/positions.py
@@ -13,10 +13,48 @@ import math
 import logging
 
 
+def open_file(filename=False):
+    """
+    Open file and load content into string.
+    
+    Returns the content as decoded string.
+    """
 
+    # Load vars from settings
+    corpus_dir 		= settings.corpus_dir
+    corpus_subdir 	= settings.corpus_subdir
+    author 		= settings.author
+    filename_addon	= settings.filename_addon
+    # If filename_prefix is not set, use author
+    filename_prefix	= settings.filename_prefix if settings.filename_prefix else author 
+    
+    # If filename is not set from command line parameters, use settings
+    if not filename:
+        if settings.corpus_subdir:
+            filename = os.path.join(corpus_dir,
+                                    corpus_subdir,
+                                    filename_prefix+filename_addon)
+        else:
+            filename = os.path.join(corpus_dir,
+                                    author,
+                                    filename_prefix+filename_addon)
+
+    # Try opening the file
+    try:
+        f = codecs.open(filename, 'rb','utf8')
+        s = f.read()
+    except IOError as err:
+        print('Cannot open {}'.format(err))
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+    else:
+        f.close()
+
+        logging.info('String succesfully read')
+        return(s)
 
 def set_filename(filename=False, ending='.txt'):
-    """ Defines the filename of the tex output file and creates dir if necessary.
+    """ Define the filename of the tex output file and create dir if necessary.
 
     Keyword Arguments:
     filename -- Optional input file.
